873df6deb27842475589a7068fe97c9350f24617
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-08-21 11:22:20 +0900

    feat: 기본 버튼 컴포넌트

--- hanarowa/src/app/component/page.tsx
+++ hanarowa/src/app/component/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/atoms';
+
 const Page = () => (
   <div>
     <h1 className='font-bold-18 text-main'>하나은행</h1>
     <h1 className='font-medium-18'>하나은행</h1>
     <h1 className='font-regular-18'>하나은행</h1>
     <h1 className='font-light-18'>하나은행</h1>
+    <Button variant='line' sizeType='xs'>
+      강의 신청하러 가기
+    </Button>
   </div>
 );
 
