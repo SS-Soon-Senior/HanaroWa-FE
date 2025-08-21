16a2330902cb67115b3b3c33e024627572bf4a94
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-08-21 13:12:28 +0900

    feat: IconButton

--- hanarowa/src/app/component/page.tsx
+++ hanarowa/src/app/component/page.tsx
@@ -1,4 +1,6 @@
-import { Button } from '@/components/atoms';
+import IcBell from '@/assets/svg/IcBell';
+import IcSearch from '@/assets/svg/IcSearch';
+import { Button, IconButton } from '@/components/atoms';
 
 const Page = () => (
   <div>
@@ -6,9 +8,11 @@ const Page = () => (
     <h1 className='font-medium-18'>하나은행</h1>
     <h1 className='font-regular-18'>하나은행</h1>
     <h1 className='font-light-18'>하나은행</h1>
-    <Button variant='line' sizeType='xs'>
+    <Button variant='line' sizeType='lg'>
       강의 신청하러 가기
     </Button>
+    <IconButton title='강좌 개설' icon={<IcBell />} />
+    <IconButton title='강좌 찾기' icon={<IcSearch />} />
   </div>
 );
 
---
+++ hanarowa/src/components/atoms/IconButton.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx';
+import { ButtonHTMLAttributes, PropsWithChildren, ReactNode } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  icon?: ReactNode;
+  disabled?: boolean;
+}
+
+const IconButton = ({
+  disabled = false,
+  className,
+  icon,
+  title,
+  children,
+  ...props
+}: PropsWithChildren<ButtonProps>) => {
+  const baseStyles =
+    'rounded-16 flex flex-col items-center justify-center gap-[0.6rem] p-[0.7rem] shadow-[0_2px_12px_0_rgba(0,0,0,0.10)]';
+
+  return (
+    <button
+      className={clsx(baseStyles, className)}
+      disabled={disabled}
+      {...props}
+    >
+      {icon}
+      <span className='font-medium-16 text-black'>{title}</span>
+      {children}
+    </button>
+  );
+};
+
+export default IconButton;
