3971364b8f4dda7e62adaea196cceaa927089a99
chaeyoung103 <thdcodud103@naver.com>
2025-08-22 17:09:10 +0900

    feat: Textarea 컴포넌트 생성

--- hanarowa/src/components/atoms/index.ts
+++ hanarowa/src/components/atoms/index.ts
@@ -6,5 +6,14 @@ import Modal from './Modal';
 import Input from './Input';
 import InputUnderline from './InputUnderline';
 import SpotButton from './SpotButton';
+import Textarea from './Textarea';
 
-export { Button, Header, IconButton, SpotButton, Input, InputUnderline, ErrorMessage, Modal };
+export {
+  Button,
+  Header,
+  IconButton,
+  SpotButton,
+  Input,
+  InputUnderline,
+  Textarea,
+};
