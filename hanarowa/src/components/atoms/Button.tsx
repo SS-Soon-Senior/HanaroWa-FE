ee13dafe236b2d1ec6a6b33d2a436ac91da733ab
JungSoEun <sod00402@konkuk.ac.kr>
2025-08-22 14:37:04 +0900

    Feat: Button/IconButton component (#2)
    * feat: 기본 버튼 컴포넌트
    
    * chore: svgr setting
    
    * feat: IconButton
    
    * chore: delete .DS_Store
    
    * fix: 크기 수정
    
    * feat: SpotButton Component

---
+++ hanarowa/src/components/atoms/Button.tsx
@@ -0,0 +1,42 @@
+import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'green' | 'disabled' | 'lightgray' | 'line';
+  sizeType?: 'lg' | 'md' | 'sm' | 'xs';
+}
+
+const Button = ({
+  variant = 'green',
+  sizeType = 'md',
+  children,
+  className,
+  ...props
+}: PropsWithChildren<ButtonProps>) => {
+  const baseStyles = 'w-full flex items-center justify-center';
+  const variantStyles = {
+    green: 'bg-main text-white',
+    disabled: 'bg-disable text-white',
+    lightgray: 'bg-gray4f6 text-gray280 border border-gray7eb',
+    line: 'bg-opacity-0  text-black border border-grayaaa',
+  };
+
+  const sizeTypeStyles = {
+    lg: 'font-bold-22 rounded-8 py-[1.7rem]',
+    md: 'font-medium-22 rounded-16 py-[1.1rem]',
+    sm: 'font-medium-20 rounded-12 py-[1.4rem]',
+    xs: 'font-medium-16 rounded-8 py-[1.1rem]',
+  };
+
+  return (
+    <button
+      type='button'
+      className={`${baseStyles} ${variantStyles[variant]} ${sizeTypeStyles[sizeType]} ${className}`}
+      disabled={variant === 'disabled'}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
