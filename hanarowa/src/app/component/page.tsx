7f6ebaceade967a3d86dd61173ce60fa707e3f7e
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-08-22 15:25:29 +0900

    chore: page.tsx 코드 삭제

--- hanarowa/src/app/component/page.tsx
+++ hanarowa/src/app/component/page.tsx
@@ -1,31 +1,28 @@
-import { Button, Header, IconButton, SpotButton } from '@/components/atoms';
+import { Button, IconButton, SpotButton } from '@/components/atoms';
 import { IcBell, IcCalendar, IcSearch } from '@svg';
 
 const Page = () => (
   <div>
-    <Header title='하나은행' back={true} />
-    <div>
-      <h1 className='font-bold-18 text-main'>하나은행</h1>
-      <h1 className='font-medium-18'>하나은행</h1>
-      <h1 className='font-regular-18'>하나은행</h1>
-      <h1 className='font-light-18'>하나은행</h1>
-      <div className='flex flex-col gap-4'>
-        <Button variant='line' sizeType='lg'>
-          강의 신청하러 가기
-        </Button>
-        <Button variant='green' sizeType='xs'>
-          강의 신청하러 가기
-        </Button>
-      </div>
-      <div className='flex flex-row gap-4'>
-        <IconButton title='강좌 개설' icon={<IcBell />} />
-        <IconButton title='내 예약' icon={<IcCalendar />} />
-        <IconButton title='강좌 찾기' icon={<IcSearch />} />
-      </div>
-      <div className='flex flex-row gap-5'>
-        <SpotButton location='춘천' title='50+ 컬처뱅크' />
-        <SpotButton location='춘천' title='50+ 컬처뱅크' />
-      </div>
+    <h1 className='font-bold-18 text-main'>하나은행</h1>
+    <h1 className='font-medium-18'>하나은행</h1>
+    <h1 className='font-regular-18'>하나은행</h1>
+    <h1 className='font-light-18'>하나은행</h1>
+    <div className='flex flex-col gap-4'>
+      <Button variant='line' sizeType='lg'>
+        강의 신청하러 가기
+      </Button>
+      <Button variant='green' sizeType='xs'>
+        강의 신청하러 가기
+      </Button>
+    </div>
+    <div className='flex flex-row gap-4'>
+      <IconButton title='강좌 개설' icon={<IcBell />} />
+      <IconButton title='내 예약' icon={<IcCalendar />} />
+      <IconButton title='강좌 찾기' icon={<IcSearch />} />
+    </div>
+    <div className='flex flex-row gap-5'>
+      <SpotButton location='춘천' title='50+ 컬처뱅크' />
+      <SpotButton location='춘천' title='50+ 컬처뱅크' />
     </div>
   </div>
 );
