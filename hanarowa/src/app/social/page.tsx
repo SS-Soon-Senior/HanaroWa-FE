895063c8000f3bf7eceef91db20421204cedcd76
doormoonn <robort123@gachon.ac.kr>
2025-08-24 18:08:48 +0900

    로그인 페이지 & 스플레쉬 페이지

--- hanarowa/src/app/social/page.tsx
+++ hanarowa/src/app/social/page.tsx
@@ -1,5 +1,6 @@
 import { IcGoogle, IcKakao, IcNaver } from '@/assets/svg';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function SocialLoginPage() {
   return (
@@ -16,29 +17,33 @@ export default function SocialLoginPage() {
           하나로와
         </p>
       </div>
-      <div className='flex w-full flex-col items-center gap-[0.1rem] pt-[6.095rem]'>
-        <button className='bg-gray-4f6 font-bold-22 flex w-full flex-row py-[1.7rem]'>
+      <div className='rounded-8 flex w-full flex-col items-center gap-[1rem] pt-[6.095rem]'>
+        <button className='bg-gray4f6 font-bold-22 flex w-full flex-row py-[1.7rem]'>
           <div className='flex flex-row gap-[1rem] pl-[5.15rem]'>
             <IcGoogle width={38} height={38} />
             <p>구글로 로그인하기</p>
           </div>
         </button>
-        <button className='bg-kakao font-bold-22 flex w-full flex-row py-[1.7rem]'>
+        <button className='bg-kakao font-bold-22 rounded-8 flex w-full flex-row py-[1.7rem]'>
           <div className='flex flex-row gap-[1rem] pl-[5.15rem]'>
             <IcKakao width={38} height={38} />
             <p>카카오로 로그인하기</p>
           </div>
         </button>
-        <button className='bg-naver font-bold-22 text-background flex w-full flex-row py-[1.7rem]'>
+        <button className='bg-naver font-bold-22 text-background rounded-8 flex w-full flex-row py-[1.7rem]'>
           <div className='flex flex-row gap-[1rem] pl-[5.15rem]'>
             <IcNaver width={38} height={38} />
             <p>네이버로 로그인하기</p>
           </div>
         </button>
       </div>
-      <p className='font-medium-18 text-gray4a9 pt-[1.6rem]'>
+
+      <Link
+        href='/login'
+        className='font-medium-18 text-gray4a9 pt-[1.6rem] underline'
+      >
         로그인/회원가입하기
-      </p>
+      </Link>
     </div>
   );
 }
