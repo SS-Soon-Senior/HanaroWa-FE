acba135a43def66d0f10501c59bb0379410f782b
doormoonn <robort123@gachon.ac.kr>
2025-08-25 09:08:05 +0900

    회원가입 완료 페이지

---
+++ hanarowa/src/app/signupend/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { IcCheck } from '@/assets/svg';
+import { Button } from '@/components/atoms';
+import Layout from '@/components/atoms/Layout';
+
+export default function SignUpSuccess() {
+  return (
+    <Layout>
+      <div className='flex flex-col justify-center'>
+        <div className='flex flex-col items-start justify-start pt-[14.1rem] pl-[2.2rem]'>
+          <h1 className='font-bold-26 text-black'>회원가입을 완료했어요!</h1>
+          <p className='font-medium-20 text-gray4a9 pt-[1.3rem]'>
+            마이페이지에서
+          </p>
+          <p className='font-medium-20 text-gray4a9'>
+            프로필을 변경할 수 있어요!
+          </p>
+        </div>
+      </div>
+      {/* 아이콘 및 중앙 공간 */}
+      <div className='mx-auto flex flex-col items-center justify-center'>
+        <div className='flex items-center justify-center pt-[7.9rem]'>
+          <IcCheck />
+        </div>
+        <div className='flex w-full items-center justify-center pt-[27.6rem]'>
+          <Button className='w-full' sizeType='lg' variant='green'>
+            강좌 신청하러 가기
+          </Button>
+        </div>
+      </div>
+    </Layout>
+  );
+}
