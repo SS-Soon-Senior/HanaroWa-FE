94efe1d216ec2c59dd715dc98cfc241fe4ae7e7d
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-08-25 13:35:35 +0900

    refactor: social페이지 리펙토링

--- hanarowa/src/app/splash/page.tsx
+++ hanarowa/src/app/splash/page.tsx
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <div className='flex h-screen flex-col items-center justify-start pt-[33vh]'>
       <IcBookByeoldol width='200' height='200' viewBox='0 0 109 109' />
-      <p className='font-bold-30 text-center text-black'>하나로와</p>
+      <p className='font-medium-30 text-center text-black'>하나로와</p>
     </div>
   );
 };
