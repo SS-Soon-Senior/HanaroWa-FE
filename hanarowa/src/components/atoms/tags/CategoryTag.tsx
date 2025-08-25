69af9a0a9a79fa68907c55dd13df799201ce8613
chaeyoung103 <thdcodud103@naver.com>
2025-08-25 14:20:40 +0900

    fix: 카테고리 태그 크기조정

--- hanarowa/src/components/atoms/tags/CategoryTag.tsx
+++ hanarowa/src/components/atoms/tags/CategoryTag.tsx
@@ -1,14 +1,11 @@
 import { CATEGORY_META, CategoryKey } from '@/constants/category';
-import { PropsWithChildren } from 'react';
 
-const CategoryTag = ({
-  category,
-}: PropsWithChildren<{ category: CategoryKey }>) => {
+const CategoryTag = ({ category }: { category: CategoryKey }) => {
   const { title, className } = CATEGORY_META[category];
 
   return (
     <span
-      className={`font-bold-14 inline-block rounded-full px-[1.6rem] py-[0.8rem] ${className}`}
+      className={`font-bold-14 inline-block h-fit w-fit rounded-full px-[1.6rem] py-[0.8rem] ${className}`}
     >
       {title}
     </span>
