067e4fba2c6348dd1bc1c4c12977f3f8b563d5bf
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-08-26 13:37:05 +0900

    fix: 내가 자주 이용하는 지점을 선택해주세요! 문구 추가

--- hanarowa/src/app/branch/page.tsx
+++ hanarowa/src/app/branch/page.tsx
@@ -17,9 +17,12 @@ const Page = () => {
 
   return (
     <Layout>
-      <div className='bg-background sticky top-0 z-20 flex w-full flex-col items-center pt-[7rem] pb-[4rem]'>
+      <div className='bg-background sticky top-0 z-20 flex w-full flex-col items-center pt-[7rem] pb-[3rem]'>
         <IcPlayByeoldol className='mx-auto' />
         <h1 className='font-bold-30 text-main text-center'>지점 선택하기</h1>
+        <h2 className='font-medium-18 text-gray4a9 text-center'>
+          내가 자주 이용하는 지점을 선택해주세요!
+        </h2>
       </div>
       <div className='overflow-scroll overflow-y-auto'>
         <div className='grid grid-cols-2 gap-x-[1.5rem] gap-y-[2.4rem] pb-12'>
