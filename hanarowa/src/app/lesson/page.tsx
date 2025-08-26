f1b60070e5590689e24bc2f1856fe20e0b432046
chaeyoung103 <thdcodud103@naver.com>
2025-08-26 17:22:35 +0900

    fix: 카테고리 선택되도록 수정

--- hanarowa/src/app/lesson/page.tsx
+++ hanarowa/src/app/lesson/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { CategoryKey } from '@/constants/category';
 import { Lessons } from '@/data/lessons';
 import {
   Layout,
@@ -8,12 +9,24 @@ import {
   CategoryFilter,
   LessonCard,
 } from '@components';
+import { useState } from 'react';
 
 const Page = () => {
+  const [selectedCategories, setSelectedCategories] = useState<CategoryKey[]>(
+    []
+  );
+
+  const handleCategoryChange = (categories: CategoryKey[]) => {
+    setSelectedCategories(categories);
+  };
+
   return (
     <Layout header={<Header title='강좌 목록' showSearchButton />}>
       <BranchFilter branchName='춘천 컬쳐뱅크' onChangeBranch={() => {}} />
-      <CategoryFilter selectedCategories={[]} onChange={() => {}} />
+      <CategoryFilter
+        selectedCategories={selectedCategories}
+        onChange={handleCategoryChange}
+      />
       <div className='grid w-full grid-cols-2 gap-[2.5rem]'>
         {Lessons.map(({ id, ...cardProps }) => (
           <LessonCard key={id} {...cardProps} />
