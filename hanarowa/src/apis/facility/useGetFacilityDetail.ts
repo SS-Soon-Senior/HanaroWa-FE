63ffe75e730776f26d95a55d96a5d85ffb3b76da
doormoonn <robort123@gachon.ac.kr>
2025-09-01 09:39:33 +0900

    dateFn utils분리, 이미지 크기 수정

--- hanarowa/src/apis/facility/useGetFacilityDetail.ts
+++ hanarowa/src/apis/facility/useGetFacilityDetail.ts
@@ -3,11 +3,6 @@ import { TanstackQueryClient } from '@apis';
 //클라이언트 컴포넌트에서 사용하는 경우
 
 const useGetFacilityDetail = (facilityId: string) => {
-  console.log('🔥 API 호출: useGetFacilityDetail', {
-    facilityId,
-    timestamp: new Date().toLocaleTimeString(),
-  });
-
   return TanstackQueryClient.useQuery('get', '/facility/detail/{facilityId}', {
     params: {
       path: {
---
+++ hanarowa/src/utils/facility.ts
@@ -0,0 +1,15 @@
+const createFacilityDate = () => {
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
+  const formatDate = (date: Date): string => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+  return { formatDate, tomorrow, today };
+};
+
+export default createFacilityDate;
