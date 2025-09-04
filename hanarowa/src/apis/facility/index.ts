0cc46e7c6e8873206865e4f3ca799bc40d8f224a
cheskt <andy9793@naver.com>
2025-09-04 09:14:41 +0900

    Fix: 시설 리스트 조회 api 수정

--- hanarowa/src/apis/facility/index.ts
+++ hanarowa/src/apis/facility/index.ts
@@ -1,6 +1,8 @@
 import useDeleteFacilityTime from './admin/useDeleteFacilityTime';
 import useFacilitylist from './admin/useFacilitylist';
+import getFacilities from './getFacilities';
 import getMyFacility from './getMyFacility';
+import { useGetFacilities } from './useGetFacilities';
 import useGetFacilityDetail from './useGetFacilityDetail';
 import usePostReserveFacility from './usePostReserveFacility';
 
@@ -8,6 +10,8 @@ export {
   usePostReserveFacility,
   useGetFacilityDetail,
   getMyFacility,
+  useGetFacilities,
+  getFacilities,
   useFacilitylist,
   useDeleteFacilityTime,
 };
