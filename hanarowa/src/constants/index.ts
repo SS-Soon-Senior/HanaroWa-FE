8fab3006f33e699b0c6da5d6d9e01df1cdd02aa5
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-09-04 19:55:53 +0900

    chore: 빌드에러

--- hanarowa/src/constants/index.ts
+++ hanarowa/src/constants/index.ts
@@ -1,6 +1,6 @@
 import { CATEGORY_META, CATEGORY_KEYS } from './category';
 import { categoryOptions, dayOptions, timeOptions } from './lesson-options';
-import { StatusKey, STATUS_META } from './status';
+import { STATUS_META } from './status';
 
 export {
   CATEGORY_META,
