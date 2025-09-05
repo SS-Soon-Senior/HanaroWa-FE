a66a54c6f704234ff9e846da75fce05b1788159f
chaeyoung103 <thdcodud103@naver.com>
2025-09-05 13:29:17 +0900

    fix: 클라이언트 컴포넌트 분리 및 토스트 추가

--- hanarowa/src/app/lesson/[lessonId]/page.tsx
+++ hanarowa/src/app/lesson/[lessonId]/page.tsx
@@ -1,15 +1,14 @@
 import getLessonDetail from '@/apis/lesson/getLessonDetailInfo';
-import postLessonApply from '@/apis/lesson/postLessonApply';
 import {
   InstructorInfo,
   LessonCurriculum,
   LessonInfo,
   LessonReview,
   LessonTimeTable,
-  Button,
   Devider,
   Header,
   Layout,
+  ApplyButton,
 } from '@/components';
 import {
   calculateDurationInWeeks,
@@ -17,7 +16,6 @@ import {
   formatStartDate,
 } from '@/utils/formatter';
 import Image from 'next/image';
-import { redirect } from 'next/navigation';
 import { use } from 'react';
 
 type Props = {
@@ -28,7 +26,6 @@ const Page = ({ params }: Props) => {
   const { lessonId } = use(params);
   const { data } = use(getLessonDetail(Number(lessonId)));
   const lessonData = data?.result;
-
   const startDate = formatStartDate(
     lessonData?.lessonGisus?.[0].duration ?? ''
   ); // "2025년 1월 1일"
@@ -39,71 +36,49 @@ const Page = ({ params }: Props) => {
     lessonData?.lessonGisus?.[0].duration ?? ''
   ); // "8주"
 
-  async function applyForLessonAction() {
-    'use server';
-
-    const lessonGisuId = lessonData?.lessonGisus?.[0].id;
-
-    const { data, error } = await postLessonApply(Number(lessonGisuId));
-    if (data?.isSuccess && !error) {
-      const message = encodeURIComponent('강좌 신청이');
-      redirect(`/complete?state=${message}`);
-    }
-
-    if (error) {
-      console.error('강좌 신청 실패:', error);
-      // 에러 처리 로직 추가 (예: 사용자에게 에러 메시지 표시)
-    }
-  }
-
   return (
     <Layout header={<Header title='강좌 상세' showBackButton />}>
-      <form className='flex w-full flex-col' action={applyForLessonAction}>
-        <div className='relative -mx-[2rem] h-[28rem] w-[calc(100%+4rem)]'>
-          <Image
-            src={lessonData?.lessonImg ?? '/images/lesson/sample-lesson.png'}
-            alt='lesson-detail-image'
-            fill
-            className='object-cover'
-          />
-        </div>
-        <LessonInfo
-          category={lessonData?.category ?? 'DIGITAL'}
-          title={lessonData?.lessonName ?? ''}
-          description={lessonData?.description ?? ''}
-          currentApplicants={
-            lessonData?.lessonGisus?.[0].currentEnrollment ?? 0
-          }
-          maxApplicants={lessonData?.lessonGisus?.[0].capacity ?? 0}
-          price={lessonData?.lessonGisus?.[0].lessonFee ?? 0}
+      <div className='relative -mx-[2rem] h-[28rem] w-[calc(100%+4rem)]'>
+        <Image
+          src={lessonData?.lessonImg ?? '/images/lesson/sample-lesson.png'}
+          alt='lesson-detail-image'
+          fill
+          className='object-cover'
         />
-        <Devider />
-        <InstructorInfo
-          name={lessonData?.instructor ?? ''}
-          content={lessonData?.instruction ?? ''}
-          rating={lessonData?.averageRating ?? 0}
-        />
-        <Devider />
-        <div className='flex w-full flex-col gap-[2rem] py-[2.5rem]'>
-          <p className='font-bold-22 text-black'>강의실</p>
-          <p className='font-medium-18 text-gray353'>303호</p>
-        </div>
-        <Devider />
-        <LessonTimeTable
-          startDate={startDate}
-          classDays={lessonTime}
-          duration={duration}
-        />
-        <Devider />
-        <LessonCurriculum
-          curriculum={lessonData?.lessonGisus?.[0].curriculums ?? []}
-        />
-        <Devider />
-        <LessonReview reviews={lessonData?.reviews ?? []} />
-        <Button sizeType='lg' type='submit'>
-          강좌 신청하기
-        </Button>
-      </form>
+      </div>
+      <LessonInfo
+        category={lessonData?.category ?? 'DIGITAL'}
+        title={lessonData?.lessonName ?? ''}
+        description={lessonData?.description ?? ''}
+        currentApplicants={lessonData?.lessonGisus?.[0].currentEnrollment ?? 0}
+        maxApplicants={lessonData?.lessonGisus?.[0].capacity ?? 0}
+        price={lessonData?.lessonGisus?.[0].lessonFee ?? 0}
+        lessonGisuId={lessonData?.lessonGisus?.[0].id ?? 0}
+      />
+      <Devider />
+      <InstructorInfo
+        name={lessonData?.instructor ?? ''}
+        content={lessonData?.instruction ?? ''}
+        rating={lessonData?.averageRating ?? 0}
+      />
+      <Devider />
+      <div className='flex w-full flex-col gap-[2rem] py-[2.5rem]'>
+        <p className='font-bold-22 text-black'>강의실</p>
+        <p className='font-medium-18 text-gray353'>303호</p>
+      </div>
+      <Devider />
+      <LessonTimeTable
+        startDate={startDate}
+        classDays={lessonTime}
+        duration={duration}
+      />
+      <Devider />
+      <LessonCurriculum
+        curriculum={lessonData?.lessonGisus?.[0].curriculums ?? []}
+      />
+      <Devider />
+      <LessonReview reviews={lessonData?.reviews ?? []} />
+      <ApplyButton lessonGisuId={lessonData?.lessonGisus?.[0].id ?? 0} />
     </Layout>
   );
 };
