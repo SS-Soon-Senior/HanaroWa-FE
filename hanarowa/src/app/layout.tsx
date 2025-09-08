dfeec1f4d5ce2c27a63d8a4f9c0fb5d37cc94fef
doormoonn <robort123@gachon.ac.kr>
2025-09-08 11:27:06 +0900

    fix: font test

--- hanarowa/src/app/layout.tsx
+++ hanarowa/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ToastProvider } from '@components';
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import { ReactNode } from 'react';
 import './globals.css';
 import Providers from './provider';
@@ -10,13 +11,19 @@ export const metadata: Metadata = {
   icons: { icon: '/favicon.svg' },
 };
 
+const myFont = localFont({
+  src: './fonts/Hana2-Medium.otf', // 폰트 파일 경로
+  variable: '--font-hana',
+  display: 'swap', // 폰트 로딩 방식 설정
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
   return (
-    <html lang='ko' suppressHydrationWarning>
+    <html lang='ko' suppressHydrationWarning className={myFont.className}>
       <body className='antialiased'>
         <Providers>{children}</Providers>
         <ToastProvider />
